0d7769c19fd289e1dd01b31ddad2bf125ab92eca
Hakeem-7 <52264207+Hakeem-7@users.noreply.github.com>
2020-02-27 11:23:24 -0600

    Intro to Time-Series

---
+++ Raw script.R
@@ -0,0 +1,21 @@
+library(car)
+# leveneTest()
+"This function takes a value x, and does things and returns things that
+take several lines to explain"
+doEverythingOften <- function(x) {
+  # Non! Comment it out! We'll just do it once for now.
+  "if (x %in% 1:9) {
+          doTenEverythings()
+     }"
+  doEverythingOnce()
+  ...
+  return(list(
+    everythingDone = TRUE, 
+    howOftenDone = 1
+  ))
+}
+
+
+install.packages("astsa")
+require(astsa)
+?astsa
